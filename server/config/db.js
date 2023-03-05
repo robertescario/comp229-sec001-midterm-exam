7be27e74d9f0783b9c02509b8cdfceecc729e0be
Bobby Escario <bobby@Bobbys-MBP.ht.home>
2023-03-04 22:41:53 -0500

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  //cloud MongoDB deployment on Atlas
+  "URI":"mongodb+srv://robertgescario:IgvPEfhQURUk479v@cluster0.3aejz5z.mongodb.net/midterm?retryWrites=true&w=majority"
+};
